27a078473cf9d73550ac3cbe42361edd519df249
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2024-09-30 15:18:54 -0300

    text-wrap

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="mx-auto max-w-4xl px-4 py-10 lg:p-2">
       <div className="mb-10 flex flex-wrap items-center gap-10">
         <Image alt="logo colorau" height={100} width={200} src="/logo.png" />
-        <h1 className="text-3xl font-bold">Material de Apoio</h1>
+        <h1 className="text-nowrap text-3xl font-bold">Material de Apoio</h1>
       </div>
       <RepertoireTable />
     </main>

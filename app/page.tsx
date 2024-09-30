763d707755602d2eab90ffd5302fde5a1bf91bdd
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2024-09-30 15:15:47 -0300

    padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-4xl">
+    <main className="mx-auto max-w-4xl p-10">
       <div className="mb-10 flex items-center gap-10">
         <Image alt="logo colorau" height={100} width={200} src="/logo.png" />
         <h1 className="text-3xl font-bold">Material de Apoio</h1>

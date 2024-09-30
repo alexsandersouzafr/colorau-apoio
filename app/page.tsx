3706ec112e4b2d7af0a064713951cae3f91280a1
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2024-09-30 15:18:44 -0300

    wrap

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="mx-auto max-w-4xl px-4 py-10 lg:p-2">
-      <div className="mb-10 flex items-center gap-10">
+      <div className="mb-10 flex flex-wrap items-center gap-10">
         <Image alt="logo colorau" height={100} width={200} src="/logo.png" />
         <h1 className="text-3xl font-bold">Material de Apoio</h1>
       </div>

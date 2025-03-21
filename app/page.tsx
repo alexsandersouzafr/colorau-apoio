ee64a0a727dcf3c041397947332239ad173e2759
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2025-03-21 17:57:02 -0300

    regimento interno

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Player from "@/components/player";
 import RepertoireTable from "@/components/repertoire-table";
+import { Compass } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +11,12 @@ export default function Home() {
         <Image alt="logo colorau" height={100} width={200} src="/logo.png" />
         <h1 className="text-nowrap text-3xl font-bold">Material de Apoio</h1>
       </div>
+      <Link href="/regimento-interno-colorau-2025.pdf">
+        <div className="flex w-fit gap-4 rounded-md border p-2 px-3">
+          <Compass />
+          Acessar Regimento Interno
+        </div>
+      </Link>
       <Player />
       <RepertoireTable />
     </main>
